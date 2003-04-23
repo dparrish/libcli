d258194baf3e829eb8f052590c0f861f2a520b6c
David Parrish <david@dparrish.com>
2003-04-23 05:23:32 +0000

    Fixed history search Require all functions to use struct cli_def * as first argument

--- libcli/libcli.h
+++ libcli/libcli.h
@@ -4,13 +4,24 @@
 #define CLI_OK		0
 #define CLI_ERROR	-1
 #define CLI_QUIT	-2
+#define MAX_HISTORY	256
 
 #include <stdio.h>
+
+struct cli_def
+{
+    int completion_callback;
+    struct cli_command *commands;
+    int (*auth_callback)(char *, char *);
+    char *banner;
+    struct unp *users;
+    char *history[MAX_HISTORY];
+};
 
 struct cli_command
 {
     char *command;
-    int (*callback)(FILE *, char *, char **, int);
+    int (*callback)(struct cli_def *, FILE *, char *, char **, int);
     int unique_len;
     char *help;
     struct cli_command *next;
@@ -18,14 +29,14 @@ struct cli_command
     struct cli_command *parent;
 };
 
-int cli_init();
-int cli_done();
-struct cli_command *cli_register_command(struct cli_command *parent, char *command, int (*callback)(FILE *, char *, char **, int), char *help);
-int cli_unregister_command(char *command);
-int cli_loop(int sockfd, char *prompt);
-void cli_set_auth_callback(int (*auth_callback)(char *, char *));
-void cli_allow_user(char *username, char *password);
-void cli_deny_user(char *username);
-void cli_set_banner(char *banner);
+struct cli_def *cli_init();
+int cli_done(struct cli_def *cli);
+struct cli_command *cli_register_command(struct cli_def *cli, struct cli_command *parent, char *command, int (*callback)(struct cli_def *, FILE *, char *, char **, int), char *help);
+int cli_unregister_command(struct cli_def *cli, char *command);
+int cli_loop(struct cli_def *cli, int sockfd, char *prompt);
+void cli_set_auth_callback(struct cli_def *cli, int (*auth_callback)(char *, char *));
+void cli_allow_user(struct cli_def *cli, char *username, char *password);
+void cli_deny_user(struct cli_def *cli, char *username);
+void cli_set_banner(struct cli_def *cli, char *banner);
 
 #endif
