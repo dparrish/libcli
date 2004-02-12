b1c5e6701683047877a69026e34b1ebe6363b0ce
fred_nerk <none@none>
2004-02-12 05:09:56 +0000

    This revision adds support for users filtering output.
    Added cli_print(). You should use this for output from all callback functions.
    Added cli_add_filter() and cli_clear_filter(). These are called internally when the user specifies | to use a filter.
    Added 3 new built-in filters - inc, begin and between.
    
    Modified clitest.c to use the new cli_print() function.
    
    Updated documentation to reflect these changes.
    
    Bumped version to 1.3.0
    
    --HG--
    extra : convert_revision : svn%3Abef2e6be-9598-11dd-8950-3d966a63a0de/trunk%4023

--- libcli/libcli.h
+++ libcli/libcli.h
@@ -18,6 +18,10 @@ struct cli_def
     struct unp *users;
     char *history[MAX_HISTORY];
     char showprompt;
+    int (*filter)(struct cli_def *cli, char *string, char *params[], int num_params);
+    int filter_param_i;
+    char **filter_param_s;
+    void *filter_data;
 };
 
 struct cli_command
@@ -42,5 +46,8 @@ void cli_deny_user(struct cli_def *cli, char *username);
 void cli_set_banner(struct cli_def *cli, char *banner);
 void cli_reprompt(struct cli_def *cli);
 void cli_regular(struct cli_def *cli, int (*callback)(struct cli_def *cli, FILE *));
+void cli_print(struct cli_def *cli, FILE *client, char *format, ...);
+void cli_add_filter(struct cli_def *cli, int (*filter)(struct cli_def *, char *, char **, int), char *params[], int num_params);
+void cli_clear_filter(struct cli_def *cli);
 
 #endif
