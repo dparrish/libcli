9c5d661cd8c8d066aa046eb598da5b81f206b934
David Parrish <david@dparrish.com>
2004-06-25 02:47:01 +0000

    Add support for privilege levels and configure states This breaks backward compatibility Test for compatibility with #ifdef LIBCLI_HAS_ENABLE Added cli_set_hostname, cli_set_promptchar, cli_set_privilege, cli_set_configmode Changed cli_file to take privilege level Changed cli_register_command to take privilege and config levels

--- libcli/libcli.h
+++ libcli/libcli.h
@@ -1,10 +1,18 @@
 #ifndef __LIBCLI_H__
 #define __LIBCLI_H__
 
-#define CLI_OK		0
-#define CLI_ERROR	-1
-#define CLI_QUIT	-2
-#define MAX_HISTORY	256
+#define CLI_OK			0
+#define CLI_ERROR		-1
+#define CLI_QUIT		-2
+#define MAX_HISTORY		256
+
+#define PRIVILEGE_UNPRIVILEGED	0
+#define PRIVILEGE_PRIVILEGED	15
+#define MODE_ANY		-1
+#define MODE_EXEC		0
+#define MODE_CONFIG		1
+
+#define LIBCLI_HAS_ENABLE	1
 
 #include <stdio.h>
 
@@ -14,10 +22,19 @@ struct cli_def
     struct cli_command *commands;
     int (*auth_callback)(char *, char *);
     int (*regular_callback)(struct cli_def *cli);
+    int (*enable_callback)(char *);
     char *banner;
     struct unp *users;
+    char *enable_password;
     char *history[MAX_HISTORY];
     char showprompt;
+    char *promptchar;
+    char *hostname;
+    char *modestring;
+    char *enable;
+    int privilege;
+    int mode;
+    int state;
     int (*filter)(struct cli_def *cli, char *string, char *params[], int num_params);
     int filter_param_i;
     char **filter_param_s;
@@ -32,6 +49,8 @@ struct cli_command
     int (*callback)(struct cli_def *, char *, char **, int);
     int unique_len;
     char *help;
+    int privilege;
+    int mode;
     struct cli_command *next;
     struct cli_command *children;
     struct cli_command *parent;
@@ -39,14 +58,20 @@ struct cli_command
 
 struct cli_def *cli_init();
 int cli_done(struct cli_def *cli);
-struct cli_command *cli_register_command(struct cli_def *cli, struct cli_command *parent, char *command, int (*callback)(struct cli_def *, char *, char **, int), char *help);
+struct cli_command *cli_register_command(struct cli_def *cli, struct cli_command *parent, char *command, int (*callback)(struct cli_def *, char *, char **, int), int privilege, int mode, char *help);
 int cli_unregister_command(struct cli_def *cli, char *command);
 int cli_loop(struct cli_def *cli, int sockfd, char *prompt);
-int cli_file(struct cli_def *cli, FILE *fh);
+int cli_file(struct cli_def *cli, FILE *fh, int privilege);
 void cli_set_auth_callback(struct cli_def *cli, int (*auth_callback)(char *, char *));
+void cli_set_enable_callback(struct cli_def *cli, int (*enable_callback)(char *));
 void cli_allow_user(struct cli_def *cli, char *username, char *password);
+void cli_allow_enable(struct cli_def *cli, char *password);
 void cli_deny_user(struct cli_def *cli, char *username);
 void cli_set_banner(struct cli_def *cli, char *banner);
+void cli_set_hostname(struct cli_def *cli, char *hostname);
+void cli_set_promptchar(struct cli_def *cli, char *promptchar);
+void cli_set_privilege(struct cli_def *cli, int privilege);
+void cli_set_configmode(struct cli_def *cli, int mode, char *config_desc);
 void cli_reprompt(struct cli_def *cli);
 void cli_regular(struct cli_def *cli, int (*callback)(struct cli_def *cli));
 void cli_print(struct cli_def *cli, char *format, ...);
