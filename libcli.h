33ce4fe9ccc827f22c85b01c9fc16658894fae41
David Parrish <dparrish@google.com>
2018-09-16 18:25:53 +0100

    Run clang-format across the code. Big reformat.
    Bump version to 1.9.8.

--- libcli.h
+++ libcli.h
@@ -7,87 +7,87 @@
 extern "C" {
 #endif
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <sys/time.h>
 
-#define LIBCLI_VERSION_MAJOR    1
-#define LIBCLI_VERISON_MINOR    9
-#define LIBCLI_VERISON_REVISION 7
-#define LIBCLI_VERSION          ((LIBCLI_VERSION_MAJOR << 16) | (LIBCLI_VERSION_MINOR << 8) | LIBCLI_VERSION_REVISION)
+#define LIBCLI_VERSION_MAJOR 1
+#define LIBCLI_VERISON_MINOR 9
+#define LIBCLI_VERISON_REVISION 8
+#define LIBCLI_VERSION ((LIBCLI_VERSION_MAJOR << 16) | (LIBCLI_VERSION_MINOR << 8) | LIBCLI_VERSION_REVISION)
 
-#define CLI_OK                  0
-#define CLI_ERROR               -1
-#define CLI_QUIT                -2
-#define CLI_ERROR_ARG           -3
+#define CLI_OK 0
+#define CLI_ERROR -1
+#define CLI_QUIT -2
+#define CLI_ERROR_ARG -3
 
-#define MAX_HISTORY             256
+#define MAX_HISTORY 256
 
-#define PRIVILEGE_UNPRIVILEGED  0
-#define PRIVILEGE_PRIVILEGED    15
-#define MODE_ANY                -1
-#define MODE_EXEC               0
-#define MODE_CONFIG             1
+#define PRIVILEGE_UNPRIVILEGED 0
+#define PRIVILEGE_PRIVILEGED 15
+#define MODE_ANY -1
+#define MODE_EXEC 0
+#define MODE_CONFIG 1
 
-#define LIBCLI_HAS_ENABLE       1
+#define LIBCLI_HAS_ENABLE 1
 
-#define PRINT_PLAIN             0
-#define PRINT_FILTERED          0x01
-#define PRINT_BUFFERED          0x02
+#define PRINT_PLAIN 0
+#define PRINT_FILTERED 0x01
+#define PRINT_BUFFERED 0x02
 
-#define CLI_MAX_LINE_LENGTH     4096
-#define CLI_MAX_LINE_WORDS      128
+#define CLI_MAX_LINE_LENGTH 4096
+#define CLI_MAX_LINE_WORDS 128
 
 struct cli_def {
-    int completion_callback;
-    struct cli_command *commands;
-    int (*auth_callback)(const char *, const char *);
-    int (*regular_callback)(struct cli_def *cli);
-    int (*enable_callback)(const char *);
-    char *banner;
-    struct unp *users;
-    char *enable_password;
-    char *history[MAX_HISTORY];
-    char showprompt;
-    char *promptchar;
-    char *hostname;
-    char *modestring;
-    int privilege;
-    int mode;
-    int state;
-    struct cli_filter *filters;
-    void (*print_callback)(struct cli_def *cli, const char *string);
-    FILE *client;
-    /* internal buffers */
-    void *conn;
-    void *service;
-    char *commandname;  // temporary buffer for cli_command_name() to prevent leak
-    char *buffer;
-    unsigned buf_size;
-    struct timeval timeout_tm;
-    time_t idle_timeout;
-    int (*idle_timeout_callback)(struct cli_def *);
-    time_t last_action;
-    int telnet_protocol;
-    void *user_context;
+  int completion_callback;
+  struct cli_command *commands;
+  int (*auth_callback)(const char *, const char *);
+  int (*regular_callback)(struct cli_def *cli);
+  int (*enable_callback)(const char *);
+  char *banner;
+  struct unp *users;
+  char *enable_password;
+  char *history[MAX_HISTORY];
+  char showprompt;
+  char *promptchar;
+  char *hostname;
+  char *modestring;
+  int privilege;
+  int mode;
+  int state;
+  struct cli_filter *filters;
+  void (*print_callback)(struct cli_def *cli, const char *string);
+  FILE *client;
+  /* internal buffers */
+  void *conn;
+  void *service;
+  char *commandname;  // temporary buffer for cli_command_name() to prevent leak
+  char *buffer;
+  unsigned buf_size;
+  struct timeval timeout_tm;
+  time_t idle_timeout;
+  int (*idle_timeout_callback)(struct cli_def *);
+  time_t last_action;
+  int telnet_protocol;
+  void *user_context;
 };
 
 struct cli_filter {
-    int (*filter)(struct cli_def *cli, const char *string, void *data);
-    void *data;
-    struct cli_filter *next;
+  int (*filter)(struct cli_def *cli, const char *string, void *data);
+  void *data;
+  struct cli_filter *next;
 };
 
 struct cli_command {
-    char *command;
-    int (*callback)(struct cli_def *, const char *, char **, int);
-    unsigned int unique_len;
-    char *help;
-    int privilege;
-    int mode;
-    struct cli_command *next;
-    struct cli_command *children;
-    struct cli_command *parent;
+  char *command;
+  int (*callback)(struct cli_def *, const char *, char **, int);
+  unsigned int unique_len;
+  char *help;
+  int privilege;
+  int mode;
+  struct cli_command *next;
+  struct cli_command *children;
+  struct cli_command *parent;
 };
 
 struct cli_def *cli_init();
@@ -113,10 +113,10 @@ int cli_set_configmode(struct cli_def *cli, int mode, const char *config_desc);
 void cli_reprompt(struct cli_def *cli);
 void cli_regular(struct cli_def *cli, int (*callback)(struct cli_def *cli));
 void cli_regular_interval(struct cli_def *cli, int seconds);
-void cli_print(struct cli_def *cli, const char *format, ...) __attribute__((format (printf, 2, 3)));
-void cli_bufprint(struct cli_def *cli, const char *format, ...) __attribute__((format (printf, 2, 3)));
+void cli_print(struct cli_def *cli, const char *format, ...) __attribute__((format(printf, 2, 3)));
+void cli_bufprint(struct cli_def *cli, const char *format, ...) __attribute__((format(printf, 2, 3)));
 void cli_vabufprint(struct cli_def *cli, const char *format, va_list ap);
-void cli_error(struct cli_def *cli, const char *format, ...) __attribute__((format (printf, 2, 3)));
+void cli_error(struct cli_def *cli, const char *format, ...) __attribute__((format(printf, 2, 3)));
 void cli_print_callback(struct cli_def *cli, void (*callback)(struct cli_def *, const char *));
 void cli_free_history(struct cli_def *cli);
 void cli_set_idle_timeout(struct cli_def *cli, unsigned int seconds);
